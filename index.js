570560bd2fd3e8691faef68e465d46764de24e0d
William Gooch <william.gooch.wg@gmail.com>
2019-02-10 19:14:35 +0000

    Add crewlist command.

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ client.on('message', msg => {
     if(msg.content[0] === '!') {
         command = msg.content.slice(1)
         switch(command) {
-            case 'help': msg.channel.send('No, I will not help you.')
+            case 'help': msg.channel.send('No, I will not help you.'); break;
+            case 'crewlist': msg.channel.send('https://docs.google.com/spreadsheets/d/1eZpNToP9dqIbEykBOal6z_OaFfaWKDTtnJXfKunebsc/edit?usp=sharing')
         }
     }
 })
