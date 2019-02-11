ec7c99c07b6a562cbb3b700b9d0cf5efc1b0bd10
William Gooch <william.gooch.wg@gmail.com>
2019-02-11 13:09:20 +0000

    improve structure

---
+++ src/ts/commands.ts
@@ -0,0 +1,35 @@
+import { Message } from 'discord.js';
+
+interface Command {
+    callback: (msg: Message) => void
+}
+let commands: { [name: string]: Command }
+
+export function addCommand(name: string, callback: (msg: Message) => void) {
+    commands[name] = { callback: callback } as Command;
+}
+
+export function performCommand(name: string, msg: Message) {
+    commands[name].callback(msg);
+}
+
+addCommand('help', (msg) => { msg.channel.send('no u') });
+addCommand('crewlist', (msg) => { msg.channel.send('https://docs.google.com/spreadsheets/d/1eZpNToP9dqIbEykBOal6z_OaFfaWKDTtnJXfKunebsc/edit?usp=sharing') });
+addCommand('tea', (msg) => { msg.channel.send('T I M E F O R T E A') });
+addCommand('gucc', (msg) => { msg.channel.send(
+`Gooch has no beginning. Gooch has no end. Gooch is infinite. Millions of years after our civilization has been eradicated and forgotten, Gooch will endure.
+Gooch is eternal. The pinnacle of evolution and existence.
+We are but rudimentary creatures of blood and flesh. We touch the mind of Gooch, fumbling in ignorance, incapable of understanding.
+Organic life is nothing but a genetic mutation, an accident. Our lives are measured in years and decades. We wither and die. Gooch is eternal. Before it, we are nothing.
+Gooch imposes order on the chaos of organic life. We exist because Gooch allows it, and we will end because Gooch demands it.
+Gooch transcends our very understanding. We cannot grasp the nature of the existence of Gooch.
+`
+)})
+addCommand('nevans', (msg) => { msg.channel.send(
+`NEVANS CAR
+    ______
+   /|_||_\`.__
+  (   _    _ _)
+  ='-(_)--(_)-'
+NEVANS CAR`
+, { code: true }) })
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,8 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+import { addCommand, performCommand } from './commands'
+
 const gApi = require('./sheetsapi.js')
 
 client.on('ready', () => {
@@ -11,24 +13,7 @@ client.on('message', msg => {
     if(msg.author.bot) return;
     if(msg.content[0] === '!') {
         var command = msg.content.slice(1)
-        switch(command) {
-            case 'help': msg.channel.send('no u'); break;
-            case 'crewlist': msg.channel.send('https://docs.google.com/spreadsheets/d/1eZpNToP9dqIbEykBOal6z_OaFfaWKDTtnJXfKunebsc/edit?usp=sharing'); break;
-            case 'tea': msg.channel.send('T I M E F O R T E A'); break;
-            case 'gucc': msg.channel.send(`Gooch has no beginning. Gooch has no end. Gooch is infinite. Millions of years after our civilization has been eradicated and forgotten, Gooch will endure.
-Gooch is eternal. The pinnacle of evolution and existence.
-We are but rudimentary creatures of blood and flesh. We touch the mind of Gooch, fumbling in ignorance, incapable of understanding.
-Organic life is nothing but a genetic mutation, an accident. Our lives are measured in years and decades. We wither and die. Gooch is eternal. Before it, we are nothing.
-Gooch imposes order on the chaos of organic life. We exist because Gooch allows it, and we will end because Gooch demands it.
-Gooch transcends our very understanding. We cannot grasp the nature of the existence of Gooch.
-`); break;
-            case 'nevans': msg.channel.send(`NEVANS CAR
-  ______
- /|_||_\`.__
-(   _    _ _)
-='-(_)--(_)-'
-NEVANS CAR`, { code: true }); break;
-        }
+        performCommand(command, msg)
     }
     if(msg.content.toLowerCase().match('\\bfuck', 'g') || msg.content.toLowerCase().match('\\bhell\\b', 'g') || msg.content.toLowerCase().match('\\bshit', 'g')) {
         msg.channel.send(msg.content
